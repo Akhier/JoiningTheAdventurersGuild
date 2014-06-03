5d64318b2a7bdaf682cdf90d0fd60d9a7b778208
akhier <akhierthedragonhearted@yahoo.com>
2014-06-03 02:34:38 -0400

    First Commit - Libtcod Working

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include "libtcod.hpp"
+int main() {
+    int playerx=40,playery=25;
+    TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+    bool done = false;
+    while ( !done ) {
+        TCOD_key_t key;
+        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+        switch(key.vk) {
+            case TCODK_UP : playery--; break;
+            case TCODK_DOWN : playery++; break;
+            case TCODK_LEFT : playerx--; break;
+            case TCODK_RIGHT : playerx++; break;
+            case TCODK_ESCAPE : done = true; break;
+            default:break;
+        }
+        TCODConsole::root->clear();
+        TCODConsole::root->putChar(playerx,playery,'@');
+        TCODConsole::flush();
+    }
+    return 0;
+}
