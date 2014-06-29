b570fa63f10a246785da001b0faf76a30d04f5df
akhier <akhierthedragonhearted@yahoo.com>
2014-06-28 22:39:09 -0400

    better argument passing

--- C_Screens.cpp
+++ C_Screens.cpp
@@ -1,9 +1,18 @@
 #include "C_Screens.h"
 
-C_Screens::getGameScreen(char** screen) {
+C_Screens::getGameScreen(char (&screen)[80][40]) {
+    screen = char[80][40];
+    for (int column = 0; column < 40; ++column) {
+        for (int row = 0; row < 80; ++row) {
 
+        }
+    }
 }
 
-C_Screens::getStartScreen(char** screen) {
+C_Screens::getStartScreen(char (&screen)[80][40]) {
+    for (int column = 0; column < 40; ++column) {
+        for (int row = 0; row < 80; ++row) {
 
+        }
+    }
 }
