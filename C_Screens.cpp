05a100335b9ce70c7df9dcd8189b7cc5e23a6ad1
akhier <akhierthedragonhearted@yahoo.com>
2014-06-28 22:51:07 -0400

    this should finish with hard coding the screens

--- C_Screens.cpp
+++ C_Screens.cpp
@@ -1,10 +1,9 @@
 #include "C_Screens.h"
 
 C_Screens::getGameScreen(char (&screen)[80][40]) {
-    screen = char[80][40];
     for (int column = 0; column < 40; ++column) {
         for (int row = 0; row < 80; ++row) {
-
+            screen[row][column] = _gamescreen[column][row];
         }
     }
 }
@@ -12,7 +11,7 @@ C_Screens::getGameScreen(char (&screen)[80][40]) {
 C_Screens::getStartScreen(char (&screen)[80][40]) {
     for (int column = 0; column < 40; ++column) {
         for (int row = 0; row < 80; ++row) {
-
+            screen[row][column] = _startscreen[column][row];
         }
     }
 }
